8df05a50fdbc4047162e5bb212ef09dc985168cf
MingHHsu <ming.hs.hsu@gmail.com>
2021-05-19 15:03:57 +0800

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 
 function App() {
   const [mcuList, setMCUList] = useState(['PI1', 'PI2']);
+  const [host, setHost] = useState('');
+  const [port, setPort] = useState(null);
   return (
     <div className="App">
       {mcuList.map((mcu) => (
@@ -10,6 +12,9 @@ function App() {
           <Link to={`/mcu/${mcu}`}>{mcu}</Link>
         </div>
       ))}
+      <input type="text" value={host} onChange={(event) => setHost(event.target.value)} placeholder="host" />
+      <input type="number" value={port} onChange={(event) => setPort(event.target.value)} placeholder="port" />
+      <button><Link to={`/mcu/host/${host}/port/${port}`}>Add MCU</Link></button>
     </div>
   );
 }
