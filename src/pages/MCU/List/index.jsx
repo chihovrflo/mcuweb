05296fe8007f06a30a1983d8538feeea39246aa8
MingHHsu <ming.hs.hsu@gmail.com>
2021-05-31 00:14:01 +0800

    add axios and example

--- src/pages/MCU/List/index.jsx
+++ src/pages/MCU/List/index.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getMCUList } from 'apis/mcu/list';
 
 export default function MCUList() {
+  const [response, setResponse] = useState('');
+  const handleGetMCUList = async () => {
+    const result = await getMCUList();
+    setResponse(result);
+  };
   return (
-    <div>MCU List</div>
+    <div>
+      MCU List
+      <button type="button" onClick={handleGetMCUList}>get mcu list</button>
+      {response}
+    </div>
   );
 }
