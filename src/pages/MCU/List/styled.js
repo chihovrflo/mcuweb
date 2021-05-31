73202e5c72b5824bbbb54986d8873495dec7b224
MingHHsu <ming.hs.hsu@gmail.com>
2021-05-31 15:15:54 +0800

    update

---
+++ src/pages/MCU/List/styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import {
+  Container,
+  Card,
+} from '@material-ui/core';
+
+export const ListRoot = styled.div``;
+
+export const ListContainer = styled(Container)``;
+
+export const ListItem = styled(Card)``;
+
+export const ListLink = styled(Link)`
+  text-decoration: none;
+`;
