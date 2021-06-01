82fd6bffd3b74f3237f7432fdd123cff4c0bd1b3
chihovrflo <tony860125@gmail.com>
2021-06-01 20:26:46 +0800

    update

--- src/pages/MCU/Detail/index.jsx
+++ src/pages/MCU/Detail/index.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 import useWebSocket from 'hooks/useWebSocket';
 import pt from 'lib/propTypes';
+import {
+  tempSetup, fanOn, fanOff, fanSetup, bulbOn, bulbOff, bulbSetup, dataRead, configFileRead,
+} from 'actions/mcu';
+import {
+  DetailInput,
+} from './styled';
 
 export default function MCUDetail({ match }) {
   const { port, host } = match.params;
   const [data, setData] = useState('');
+  const [temp, setTemp] = useState('');
+  const [fan, setFan] = useState('');
+  const [bulb, setBulb] = useState('');
   const wsRef = useWebSocket({
     onOpen: (ws) => {
       console.log('ws is opened!');
@@ -14,17 +23,51 @@ export default function MCUDetail({ match }) {
       }));
     },
     onClose: () => console.log('close'),
-    onMessage: (ws, event) => setData(event.data),
+    onMessage: (ws, event) => {
+      setData(event.data);
+    },
   });
-  const handleClick = () => {
+
+  const handleClick = (event) => () => {
     if (wsRef.current) {
-      wsRef.current.send('FanOn');
+      wsRef.current.send(JSON.stringify(event));
     }
   };
+  const handleTemp = (event) => {
+    setTemp(event.target.value);
+  };
+  const handleFan = (event) => {
+    setFan(event.target.value);
+  };
+  const handleBulb = (event) => {
+    setBulb(event.target.value);
+  };
+
   return (
     <div>
-      {data}
-      <button type="button" onClick={handleClick}>Send</button>
+      <div>
+        <DetailInput label="Temp" value={temp} onChange={handleTemp} />
+        <button type="button" onClick={handleClick(tempSetup(temp))}>Send</button>
+      </div>
+      <div>
+        <button type="button" onClick={handleClick(fanOn())}>FanOn</button>
+        <button type="button" onClick={handleClick(fanOff())}>FanOff</button>
+        <DetailInput label="Fan" value={fan} onChange={handleFan} />
+        <button type="button" onClick={handleClick(fanSetup(fan))}>Send</button>
+      </div>
+      <div>
+        <button type="button" onClick={handleClick(bulbOn())}>BulbOn</button>
+        <button type="button" onClick={handleClick(bulbOff())}>BulbOff</button>
+        <DetailInput label="Bulb" value={bulb} onChange={handleBulb} />
+        <button type="button" onClick={handleClick(bulbSetup(bulb))}>Send</button>
+      </div>
+      <div>
+        <button type="button" onClick={handleClick(dataRead())}>DataRead</button>
+        <button type="button" onClick={handleClick(configFileRead())}>ConfigFileRead</button>
+      </div>
+      <div>
+        {data}
+      </div>
     </div>
   );
 }
---
+++ src/pages/MCU/Detail/styled.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+import {
+  TextField,
+} from '@material-ui/core';
+
+export const DetailInput = styled(TextField)``;
